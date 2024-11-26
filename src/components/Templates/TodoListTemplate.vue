dbbbf6a0522fbdd910d4452e1be08b222c0a9216
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-26 18:20:32 +0900

    fix todo list

--- src/components/Templates/TodoListTemplate.vue
+++ src/components/Templates/TodoListTemplate.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { inject } from 'vue'
-import AddTodo from '../Organisms/AddTodo.vue'
+import BaseLayout from '../Organisms/BaseLayout.vue'
 import TodoList from '../Organisms/TodoList.vue'
 import InputForm from '../Atoms/InputForm.vue'
 
@@ -8,43 +8,26 @@ const searchKeyword = inject('searchKeyword')
 </script>
 
 <template>
-  <div class="container">
-    <h1 class="title">
-      Todo List
-    </h1>
-  </div>
-  <!-- Todo追加エリア -->
-  <section class="common">
-    <AddTodo />
-  </section>
-  <!-- Todo フォームエリア -->
-  <section class="common">
-    <InputForm
-      v-model="searchKeyword"
-      :placeholder="`Search Keyword`"
-    />
-  </section>
-  <!-- Todo 一覧表示エリア -->
-  <section class="common">
-    <TodoList />
-  </section>
+  <BaseLayout title="TodoList">
+    <div className="container">
+      <section class="area">
+        <InputForm v-model="searchKeyword" :placeholder="`Search Keyword`" />
+      </section>
+      <!-- Todo 一覧表示エリア -->
+      <section class="area">
+        <TodoList />
+      </section>
+    </div>
+  </BaseLayout>
 </template>
 
 <style scoped>
 .container {
-  width: 50%;
-  margin: 80px auto;
-}
-
-.title {
-  text-align: center;
-  font-family: 'Times New Roman', Times, serif;
-  color: #fff;
-  font-size: 48px;
-}
-
-.common {
   width: 80%;
   margin: 40px auto;
+}
+
+.area {
+  margin-top: 40px;
 }
 </style>
